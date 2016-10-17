e7bdf15187377f4a899c1f7ba32760f8ce1cd1d1
Sheharyar Naseer <hello@sheharyar.me>
2016-10-17 19:20:26 +0500

    Created Protobuf module with decode methods

---
+++ lib/explay/protobuf.ex
@@ -0,0 +1,37 @@
+defmodule ExPlay.Protobuf do
+  @moduledoc """
+  Wrapper Module for encoding/decoding protobuf data.
+
+  It's important that the npm module `protobufjs` is available to the
+  project. Currently no Elixir/Erlang package supports Protobuf v2.
+  """
+
+  @protobuf_file "./lib/proto/googleplay.proto"
+  @inspect_limit 10_000
+
+
+
+  @doc "Decodes protobuf data from an HTTPResponse"
+  def decode(%HTTPoison.Response{body: body}, type) do
+    decode(body, type)
+  end
+
+
+  @doc "Decodes protobuf data according to the specified type"
+  def decode(data, type) do
+    binary_list = :binary.bin_to_list(data)
+
+    {out, _exit} = System.cmd("node", ["-e", """
+      var protobuf     = require('protobufjs');
+      var builder      = protobuf.loadProtoFile('#{@protobuf_file}');
+      var module       = builder.build('#{type}');
+
+      var buffer       = new Buffer( #{ inspect(binary_list, limit: @inspect_limit) } );
+      var decodedData  = module.decode(buffer);
+
+      console.log(JSON.stringify(decodedData));
+    """])
+
+    Poison.decode!(out)
+  end
+end
